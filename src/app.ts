bb9b11a31ef419608a2cba539a4de12da3ffd46d
AlexHff <hello.alex.buzz@gmail.com>
2019-12-08 17:37:33 +0100

    Initialized db and user class

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import express from "express";
 import path from "path";
+import dotenv from "dotenv";
+import { User } from "./models/User";
+
+dotenv.config();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../public")))
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.set("port", process.env.PORT || 8000);
+app.set("port", process.env.PORT);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 
 app.get("/", (req: any, res: any) => {
   res.write('Hello world');
+  new User("alex", "alex@google.com", "1234");
   res.end();
-})
+});
 
 app.get("/hello/:name", (req: any, res: any) => {
   res.render("hello.ejs", {name: req.params.name});
---
+++ src/db.ts
@@ -0,0 +1,15 @@
+import level from "level";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+var dbPath = process.env.DB_PATH;
+var options = {    
+  keyEncoding: "binary",  
+  valueEncoding: "json"  
+};
+var db = level(dbPath, options);
+
+export default db;
+
